a83ca5f68bd0d86c115e28c7b55a0c6610dd94da
Janahvi28 <janahvithantri28@gmail.com>
2025-08-28 20:37:45 +0530

    Add files via upload

---
+++ SecurityRules.java
@@ -0,0 +1,10 @@
+package transport;
+
+public class SecurityRules {
+
+	public SecurityRules() {}
+	public static boolean canFly(String place) {
+		return false;
+		// TODO Auto-generated constructor stub
+	}
+}
